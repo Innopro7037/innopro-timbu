e5272f28d5163eba63e84f212c8e2f2fbdc356f0
Innocent Prosper <innopro7037@gmail.com>
2024-07-08 15:03:31 +0100

    push

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -7,6 +7,9 @@ const Hero = () => {
       <div className="hero-text">
         <h1>Explore the latest and best of <span>Gentle men’s watch </span></h1>
       </div>
+      <div className="her-text">
+        <h1>Explore the latest <br /> and best of <span>Gentle <br /> men’s watch </span></h1>
+      </div>
       <div className="hero-img">
         <img src={man} alt="Model" />
       </div>
